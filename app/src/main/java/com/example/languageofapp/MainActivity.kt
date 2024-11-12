f02ee42c7da851dcf7228d8949df11a6af794bd5
4ASIM <masim4872@gmail.com>
2024-11-12 10:48:59 +0500

    second commit

--- app/src/main/java/com/example/languageofapp/MainActivity.kt
+++ app/src/main/java/com/example/languageofapp/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.example.languageofapp
-
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.example.languageofapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.customButton.setOnClickListener {
+            val intent = Intent(this, Dashboard::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
